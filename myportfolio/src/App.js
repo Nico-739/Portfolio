ba0d0b5736f0ce33f9d5e649346ecde74cfe9097
Nicolaj <nicolajhaueter72@gmail.com>
2023-05-26 13:42:18 +0200

    Testing if app works with title

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Nico739</h1>
       </header>
     </div>
   );
